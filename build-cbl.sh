9a4fcc4c81a8bf59aa1d74eb0ea6b31c548ea557
R-L-T-Y <R-L-T-Y@users.noreply.github.com>
2020-05-26 17:44:01 +0800

    CI: use ccache to accelerate CI build

--- build-cbl.sh
+++ build-cbl.sh
@@ -5,13 +5,18 @@ echo "deb http://archive.ubuntu.com/ubuntu eoan main" | sudo tee /etc/apt/source
 sudo apt-get update
 sudo apt-get -y --no-install-recommends install bison flex libc6 libstdc++6 ccache libfl-dev
 
+export USE_CCACHE=1
+export CCACHE_EXEC=/usr/bin/ccache
+##export CCACHE_COMPRESS=1
+
+ccache -M 4G
+##ccache --show-stats
 mkdir -p $GITHUB_WORKSPACE/TC
 cd $GITHUB_WORKSPACE/TC
 wget 'https://github.com/kdrag0n/proton-clang-build/releases/download/20200117/proton_clang-11.0.0-20200117.tar.zst'
 tar -I zstd -xf proton_clang-11.0.0-20200117.tar.zst
 mv proton_clang-11.0.0-20200117/* ./
 echo "unarchived!"
-
 
 cd $GITHUB_WORKSPACE/kernel
 args="-j$(nproc --all) \
@@ -20,22 +25,20 @@ args="-j$(nproc --all) \
     CLANG_TRIPLE=aarch64-linux-gnu- \
     DTC_EXT=dtc \
     CROSS_COMPILE=$GITHUB_WORKSPACE/TC/bin/aarch64-linux-gnu- \
-    CC=$GITHUB_WORKSPACE/TC/bin/clang"
+    CC=/home/runner/work/android_kernel_xiaomi_sm7250/android_kernel_xiaomi_sm7250/TC/bin/clang "
+
 
 echo "Make defconfig"
 make ${args} picasso_user_defconfig
 echo "Make defconfig done, start Make"
-make ${args}
-
+make ${args}  CC='ccache /home/runner/work/android_kernel_xiaomi_sm7250/android_kernel_xiaomi_sm7250/TC/bin/clang'
+ccache --show-stats
 echo "Make Done, packaging now"
 
 cp ./out/arch/arm64/boot/Image.gz-dtb ./AnyKernel3/
 cp ./out/arch/arm64/boot/dtbo.img ./AnyKernel3/
 
-
 #still needed?
 cd AnyKernel3/
 zip -r9 AnyKernel3.zip *
 mv AnyKernel3.zip ../
-
-    
